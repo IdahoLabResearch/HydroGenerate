4fcf36cc8396a1cc3a6959d818fde293687a0921
Juan F. Gallego-Calderon <juan.gallegocalderon@inl.gov>
2024-10-10 10:56:52 -0600

    Implemented pytest framework for calculate_potential

---
+++ tests/test_hg.py
@@ -0,0 +1,11 @@
+from HydroGenerate.utils.api_call import *
+from HydroGenerate.hydropower_potential import *
+
+def test_calculate_potential():
+    flow = 8000 # given flow, in cfs
+    head = 20 # head, in ft
+    power = None
+
+    hp = calculate_hp_potential(flow= flow, head= head, rated_power= power, system_efficiency= 0.7)
+
+    assert round(hp.rated_power, 0) == 9483
